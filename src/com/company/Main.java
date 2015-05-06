9ad8b64f07520b1821498f069d5924ad0d5901fb
clara <clara@clara.com>
2015-05-06 11:43:36 -0500

    JTabbedPane with one class per tab Note layout manager for main JFrame can't be the default

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainForm form = new MainForm();
+        //form has references (directly or indirectly) to all the GUI components.
+
+
+
+    }
+}
